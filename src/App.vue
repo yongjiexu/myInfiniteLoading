3ff011d29d5b824d9b65d254282057708f6638b6
yongjie.xu <13255288587@163.com>
2018-04-09 22:23:06 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <MyInfiniteLoading></MyInfiniteLoading>
+  </div>
+</template>
+
+<script>
+import MyInfiniteLoading from './components/MyInfiniteLoading'
+
+export default {
+  name: 'App',
+  components: {
+    MyInfiniteLoading
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/MyInfiniteLoading.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="container">
+    <ul>
+      <li v-for="(item,index) in displayData" :key="index">item {{item}}</li>
+    </ul>
+    <!--<div class="load-more">loading...</div>-->
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MyInfiniteLoading',
+    data () {
+      return {
+        displayData: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+  div#container
+    width: 100%
+    height: 100vh
+    overflow-y auto
+    ul
+      display: block
+      width: 100%
+      height: 100%
+      list-style: none
+      li
+        text-decoration none
+        width: 100%
+        height: 44px
+        font-size 14px
+        line-height 44px
+        padding-left 15px
+        text-align left
+        border-bottom 1px solid #ddd
+        background: #fff
+    div
+      width: 100%
+      height: 44px
+      text-align: center
+      line-height: 44px
+      background: #fff
+      border-top: none
+      color: #48B884
+</style>
