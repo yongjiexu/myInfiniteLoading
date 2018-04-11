653499f5a6138d319c04ae7bbb50b2eb7854bfe0
yongjie.xu <13255288587@163.com>
2018-04-11 19:56:37 +0800

    实现针对大数据量场景的优化

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,62 @@
 <template>
   <div id="app">
-    <MyInfiniteLoading></MyInfiniteLoading>
+    <header>
+      <h1 class="title">vue-list</h1>
+      <p>A solution to build an infinite load more list component.</p>
+      <p><a class="button" href="https://github.com/hejianxian/vue-list">Code on Github</a> / <a class="button"
+                                                                                                 href="https://github.com/hejianxian">My
+        profile</a></p>
+    </header>
+    <div class="content">
+      <div class="preview">
+        <div class="preview-content">
+          <MyInfiniteLoading :list.sync='list' :dispatchData="setData"></MyInfiniteLoading>
+        </div>
+      </div>
+      <div class="setting">
+        <h3>Scroll info</h3>
+        <p>Total items: <span>{{list.length}}</span></p>
+        <p>Item height: <span>{{data.height}}px</span></p>
+        <p>Above items: <span>{{data._above}}</span></p>
+        <p>Below items: <span>{{data._below}}</span></p>
+        <p>Rows in window: <span>{{data.displayCount + 1}}-{{(data.displayCount + data._rowsInWindow) > list.length ? list.length: (data.displayCount + data._rowsInWindow)}}</span>
+        </p>
+        <p><span>{{data._rowsInWindow * 4}}</span> items from <span>{{data.from}}</span> to <span>{{data.to}}</span></p>
+        <p>Top height: <span>{{data.lineTopHeight}}px</span></p>
+        <p>Bottom Height: <span>{{data.lineBottomHeight}}px</span></p>
+        <p>Will load more items: <span>{{!data.canLoadmore}}</span></p>
+        <br>
+        <p>
+          <span>You can open the developer tools and observe the changes in the elements.</span>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import MyInfiniteLoading from './components/MyInfiniteLoading'
+  import MyInfiniteLoading from 'components/MyInfiniteLoading'
+
+  let COUNT = 1
 
   export default {
-    name: 'App',
+    name: 'app',
     data () {
       return {
-        listData: []
+        list: [],
+        data: {}
+      }
+    },
+    created () {
+      for (var i = 0; i < 200; i++) {
+        this.list.push({
+          title: 'item ' + COUNT++
+        })
+      }
+    },
+    methods: {
+      setData (data) {
+        this.data = data
       }
     },
     components: {
@@ -20,6 +65,62 @@
   }
 </script>
 
-<style>
+<style lang="stylus">
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
 
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    header {
+      width: 100%;
+      padding: 30px 0;
+      margin-bottom: 20px;
+      .title {
+        font-size: 46px;
+        line-height: 20px;
+        color: #48B884;
+      }
+      p {
+        color: #7F8C8D;
+        .button {
+          color: #48B884;
+        }
+      }
+    }
+    .content {
+      display: flex;
+      width: 800px;
+      height: 650px;
+      margin: 0 auto;
+      & > div {
+        flex: 1;
+        height: 100%;
+      }
+      .preview {
+        background: url("~assets/iphone.png") center center no-repeat
+        background-size: 324px 642px;
+        position: relative;
+        .preview-content {
+          width: 256px;
+          height: 464px;
+          position: absolute;
+          left: 72px;
+          top: 91px;
+          background: #fff;
+          overflow-y: scroll;
+        }
+      }
+      .setting {
+        span {
+          color: #48B884;
+        }
+      }
+    }
+  }
 </style>
--- src/components/MyInfiniteLoading.vue
+++ src/components/MyInfiniteLoading.vue
@@ -1,153 +1,197 @@
-<template>
-  <div id="my-infinite-loading-wrapper">
-    <div class="scroll-container" @scroll="handleScroll">
-      <ul>
-        <li v-for="(item,index) in dataPool" :key="index">item {{item}}</li>
-      </ul>
-    </div>
+<template lang="html">
+  <div class="vue-list" @scroll="handleScroll">
+    <ul :style="{paddingTop: lineTopHeight +'px',paddingBottom: lineBottomHeight +'px'}">
+      <li v-for="(item,index) in previewList" :key="index">{{item.title}}</li>
+    </ul>
+    <div class="load-more-gif">loading...</div>
   </div>
 </template>
 
 <script>
-  /*
-   * 频率控制 返回函数连续调用时，fn 执行频率限定为每多少时间执行一次
-   * @param fn {function}  需要调用的函数
-   * @param delay  {number}    延迟时间，单位毫秒
-   * @param immediate  {bool} 给 immediate参数传递false 绑定的函数先执行，而不是delay后执行。
-   * @return {function}实际调用函数
-   */
-  function throttle (fn, delay) {
-    var now, lastExec, timer, context, args //eslint-disable-line
 
-    var execute = function () {
-      fn.apply(context, args)
-      lastExec = now
-    }
-
-    return function () {
-      context = this
-      args = arguments
-
-      now = Date.now()
-
-      if (timer) {
-        clearTimeout(timer)
-        timer = null
-      }
-
-      if (lastExec) {
-        var diff = delay - (now - lastExec)
-        if (diff < 0) {
-          execute()
-        } else {
-          timer = setTimeout(() => {
-            execute()
-          }, diff)
-        }
-      } else {
-        execute()
-      }
-    }
-  }
-
+  let COUNT = 1
   export default {
-    name: 'MyInfiniteLoading',
+    name: 'vue-list',
+    props: {
+      list: {
+        type: Array,
+        required: true,
+        default: function () {
+          return []
+        },
+        twoWays: true
+      },
+      height: {
+        type: Number,
+        default: 44
+      },
+      canScroll: {
+        type: Boolean,
+        default: true
+      },
+      // todo ?
+      dispatchData: {
+        type: Function
+      }
+    },
     data () {
       return {
-        // 存放所有可能要展示的数据
-        dataPool: [],
-        scrollContainer: undefined
-      }
-    },
-    props: {
-      distance: {
-        type: Number,
-        required: false,
-        default: 50
-      }
-    },
-    computed: {
-      // 存放当前要展示的数据
-      displayData: function () {
-        return this.dataPool.slice(0, 30)
-      }
-    },
-    created () {
-      for (let i = 0; i < 15; i++) {
-        this.dataPool.push(i)
+        // 上一次滚动的距离
+        lastScrollTop: null,
+        distance: 44,
+        // 屏幕上面未渲染的元素的高度和
+        lineTopHeight: 0,
+        // 屏幕下面未渲染的元素的高度和
+        lineBottomHeight: 0,
+        canLoadmore: true,
+        // 当前渲染的数据
+        previewList: [],
+        displayCount: 0
       }
     },
     mounted () {
-      this.scrollContainer = document.querySelector('.scroll-container')
-      this.init()
+      this.initData()
+      this.handleScroll()
     },
     methods: {
-      /**
-       做一些初始化工作
-       */
-      init () {
-
-      },
-      /**
-       * 处理滚动事件，检测是否应该加载更多
-       */
-      _handleScroll () {
-        if (this.checkBottomReached()) {
-          this.loadMore()
-        }
+      initData () {
+        // init all data
+        // 窗口中显示的item数，向上取整
+        this._rowsInWindow = Math.ceil(this.$el.offsetHeight / this.height)
+        //  窗口上面未显示的item数
+        this._above = this._rowsInWindow * 2
+        //  窗口下面未显示的item数
+        this._below = this._rowsInWindow
+        // 最大高度,当两次相邻滚动之差绝对值>=_max,则重新组织数据
+        this._max = this._rowsInWindow * this.height
       },
       handleScroll () {
-        throttle(this._handleScroll, 100)()
-      },
-      /**
-       * 检查是否滚动到底部
-       * @returns {boolean}
-       */
-      checkBottomReached () {
-        return this.scrollContainer.scrollTop + this.scrollContainer.offsetHeight + this.distance >= this.scrollContainer.scrollHeight
-      },
-      // 模拟网络操作，加载更多数据
-      loadMore () {
-        setTimeout(() => {
-          const temp = []
-          for (let i = this.dataPool.length + 1; i <= this.dataPool.length + 10; i++) {
-            temp.push(i)
+        // 缓存当前的滚动高度
+        let _scrollTop = this.$el.scrollTop
+        // 缓存内部滚动元素即ul的高度
+        let _height = this.$el.querySelectorAll('ul')[0].offsetHeight
+        // 缓存容器高度
+        let _contentHeight = this.$el.offsetHeight
+
+        // Counts the number of rows on the current screen
+        // 计算当前屏幕显示的item数目，用于展示数据。不是实现功能必须的量
+        if (_scrollTop / this.height - Math.floor(_scrollTop / this.height) > 0.5) {
+          this.displayCount = Math.ceil(_scrollTop / this.height)
+        } else {
+          this.displayCount = Math.floor(_scrollTop / this.height)
+        }
+
+        // if the maximum height is exceeded, reset the previewList
+        if (this.lastScrollTop === null || Math.abs(_scrollTop - this.lastScrollTop) > this._max) {
+          this.lastScrollTop = _scrollTop
+        } else {
+          if (this.to === this.list.length && _height - _scrollTop - _contentHeight < this.distance) {
+            this.canScroll && this.loadMore(this.from, this.to)
           }
-          this.dataPool = this.dataPool.concat(temp)
-        }, 10)
+          return
+        }
+
+        // get from and to count
+        let _from = parseInt(_scrollTop / this.height) - this._above
+        if (_from < 0) {
+          _from = 0
+        }
+        let _to = _from + this._above + this._below + this._rowsInWindow
+        if (_to > this.list.length) {
+          _to = this.list.length
+        }
+        this.from = _from
+        this.to = _to
+
+        // set top height and bottom height
+        this.lineTopHeight = _from * this.height
+        this.lineBottomHeight = (this.list.length - _to) * this.height
+
+        // dispatch data
+        if (typeof this.dispatchData === 'function') {
+          this.dispatchData(this)
+        }
+
+        // 重新填充要渲染的数据
+        this.resetPreviewList(_from, _to)
+
+        // 填充数据后检查是否要加载新数据
+        this.$nextTick(() => {
+          let _scrollTop = this.$el.scrollTop
+          let _height = this.$el.querySelectorAll('ul')[0].offsetHeight
+          let _contentHeight = this.$el.offsetHeight
+
+          if (_to === this.list.length && _height - _scrollTop - _contentHeight < 0) {
+            this.canScroll && this.loadMore(this.from, this.to)
+          }
+        })
+      },
+      loadMore (from, to) {
+        if (!this.canLoadmore) return
+        this.canLoadmore = false
+        // fetch mock 模拟异步获取数据的操作
+        setTimeout(() => {
+          for (var i = 0; i < 200; i++) {
+            this.list.push({
+              title: 'item ' + COUNT++
+            })
+          }
+          // _from、 _to, todo 为了处理数据不能填满屏幕的情况？
+          let _from = from
+          let _to = to + this._below
+          this.resetPreviewList(_from, _to)
+          this.lineBottomHeight = (this.list.length - _to) * this.height
+          this.handleScroll()
+
+          this.canLoadmore = true
+        }, 2000)
+      },
+      resetPreviewList (from, to) {
+        // reset previewList
+        this.previewList = []
+        for (; from < to; from++) {
+          this.previewList.push(this.list[from])
+        }
       }
-    }
+    },
+    components: {}
   }
 </script>
 
-<style scoped lang="stylus">
-  #my-infinite-loading-wrapper
-    width: 100%
-    height: 100vh
-    .scroll-container
-      width: 100%
-      height: 700px
-      overflow-y scroll
-      outline: 1px solid red
-      ul
-        width: 100%
-        list-style: none
-        li
-          text-decoration none
-          width: 100%
-          height: 44px
-          font-size 14px
-          line-height 44px
-          padding-left 15px
-          text-align left
-          border-bottom 1px solid #ddd
-          background: #fff
-      div
-        width: 100%
-        height: 44px
-        text-align: center
-        line-height: 44px
-        background: #fff
-        border-top: none
-        color: #48B884
+<style lang="stylus">
+  .vue-list {
+    width: 100%;
+    height: 100%;
+    overflow-y: auto;
+    &::scroll-bar {
+      width: 0;
+    }
+    ul {
+      margin: 0;
+      padding: 0;
+      li {
+        text-decoration: none;
+        height: 44px;
+        font-size: 14px;
+        line-height: 3;
+        text-align: left;
+        padding-left: 15px;
+        border-bottom: 1px solid #ddd;
+        box-sizing: border-box;
+        background: #fff;
+        &.line-top, &.line-bottom {
+          border-bottom: 0;
+        }
+      }
+    }
+    .load-more-gif {
+      width: 100%;
+      height: 44px;
+      text-align: center;
+      line-height: 44px;
+      background: #fff;
+      border-top: none;
+      color: #48B884;
+    }
+  }
 </style>
